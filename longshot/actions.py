d3c3256aee61c402c6b08cc887293b7bb2aff025
Arthur Lucas <alucas@strathcom.com>
2015-04-16 15:32:23 -0600

    Throw exception if specifying unknown action to action factory.

--- longshot/actions.py
+++ longshot/actions.py
@@ -8,7 +8,14 @@ def text_action(self, path, element_specifier, expected_value):
     expected_value = expected_value.encode("ascii", "ignore").lower().strip()
     actual_value = element.text.encode("ascii", "ignore").lower().strip()
 
-    self.assertEqual(expected_value, actual_value)
+    if expected_value.startswith('^'):
+        expected_value = expected_value[1:]
+        self.assertTrue(actual_value.startswith(expected_value))
+    elif expected_value.startswith('~'):
+        expected_value = expected_value[1:]
+        self.assertIn(expected_value, actual_value)
+    else:
+        self.assertEqual(expected_value, actual_value)
 
 
 def create_action(path, element_specifier, action, expected_value):
@@ -16,3 +23,5 @@ def create_action(path, element_specifier, action, expected_value):
         def _text_action_proxy(self):
             return text_action(self, path, element_specifier, expected_value)
         return _text_action_proxy
+    else:
+        raise ValueError("Unknown action: " + action)
--- tests/test_actions.py
+++ tests/test_actions.py
@@ -7,16 +7,25 @@ from longshot.actions import create_action
 
 class ActionFactoryTestCase(unittest.TestCase):
 
+    def setUp(self):
+        self.mock_testcase = mock.MagicMock(**{
+            'assertEqual': self.assertEqual,
+            'assertIn': self.assertIn,
+            'assertTrue': self.assertTrue
+        })
+
     def test_should_create_text_action(self):  # FIXME: Make this less crazy
         test_config = ('/', 'h1', 'text', 'Hello World!')
-        text_action_proxy = create_action(*test_config)
-
-        mock_testcase = mock.MagicMock(**{
-            'browser.find.return_value.text.encode.return_value': test_config[3],
-            'assertEqual': self.assertEqual
+        self.mock_testcase.configure_mock(**{
+            'browser.find.return_value.text.encode.return_value': test_config[3]
         })
-        text_action_proxy(mock_testcase)
 
+        text_action_proxy = create_action(*test_config)
+        text_action_proxy(self.mock_testcase)
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_should_throw_error_if_unknown_action(self):
+        test_config = ('/', 'h1', 'INVALID-VALUE-IS-HERE', 'Hello World!')
+
+        with self.assertRaises(ValueError):
+            create_action(*test_config)
+
