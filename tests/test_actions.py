9ccbd0ae99dee5c7fa797ff9a4bf16385ee3e3f8
Arthur Lucas <alucas@strathcom.com>
2015-04-16 15:42:15 -0600

    Added tests for text action, contains and starts with operators.

--- tests/test_actions.py
+++ tests/test_actions.py
@@ -2,7 +2,7 @@ import unittest
 
 import mock
 
-from longshot.actions import create_action
+from longshot.actions import create_action, text_action
 
 
 class ActionFactoryTestCase(unittest.TestCase):
@@ -29,3 +29,50 @@ class ActionFactoryTestCase(unittest.TestCase):
         with self.assertRaises(ValueError):
             create_action(*test_config)
 
+
+class TextActionTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.mock_testcase = mock.MagicMock(**{
+            'assertEqual': self.assertEqual,
+            'assertIn': self.assertIn,
+            'assertTrue': self.assertTrue
+        })
+
+    def test_startswith_value(self):  # FIXME: Make this less crazy
+        page_text = "Hello World! This is a test."
+        test_config = ('/', 'h1', '^Hello World!')
+        self.mock_testcase.configure_mock(**{
+            'browser.find.return_value.text.encode.return_value': page_text
+        })
+
+        text_action(self.mock_testcase, *test_config)
+
+    def test_startswith_value_fails(self):  # FIXME: Make this less crazy
+        page_text = "Hello World, this should fail."
+        test_config = ('/', 'h1', '^Hello World!')
+        self.mock_testcase.configure_mock(**{
+            'browser.find.return_value.text.encode.return_value': page_text
+        })
+
+        with self.assertRaises(AssertionError):
+            text_action(self.mock_testcase, *test_config)
+
+    def test_contains_value(self):  # FIXME: Make this less crazy
+        page_text = "Hello World! This is a test."
+        test_config = ('/', 'h1', '~This')
+        self.mock_testcase.configure_mock(**{
+            'browser.find.return_value.text.encode.return_value': page_text
+        })
+
+        text_action(self.mock_testcase, *test_config)
+
+    def test_contains_value_fails(self):  # FIXME: Make this less crazy
+        page_text = "Hello World! This is a test."
+        test_config = ('/', 'h1', '~That')
+        self.mock_testcase.configure_mock(**{
+            'browser.find.return_value.text.encode.return_value': page_text
+        })
+
+        with self.assertRaises(AssertionError):
+            text_action(self.mock_testcase, *test_config)
